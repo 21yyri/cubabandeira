16527b14f52cf6aebfc53073c26da7fc3af41eea
Yurii <yuritc267@gmail.com>
2025-01-29 09:40:39 -0300

    Update cuba.py

--- cuba.py
+++ cuba.py
@@ -1,6 +1,8 @@
 import turtle
+import time
 
 # Bandeira de Cuba
+# INTRODUÇÃO A PROGRAMAÇÂO, INFO1M
 # Feito por Caio Wesley, Pedro Lucas e Yuri Teixeira
 
 t = turtle.Turtle()
@@ -9,66 +11,79 @@ t.speed(10)
 t.fillcolor("blue")
 t.begin_fill()
 for i in range(2):
-  t.forward(100)
+  t.forward(300)
   t.left(90)
-  t.forward(65)
+  t.forward(195)
   t.left(90)
 t.end_fill()
 
 t.up()
 t.left(90)
-t.forward(13)
+t.forward(39)
 t.down()
 
 t.fillcolor("white")
 t.begin_fill()
 for i in range(2):
-  t.forward(13)
+  t.forward(39)
   t.right(90)
-  t.forward(100)
+  t.forward(300)
   t.right(90)
 t.end_fill()
 
 t.up()
-t.forward(26)
+t.forward(78)
 t.right(90)
 t.down()
 
 t.fillcolor("white")
 t.begin_fill()
 for i in range(2):
-  t.forward(100)
+  t.forward(300)
   t.left(90)
-  t.forward(13)
+  t.forward(39)
   t.left(90)
 t.end_fill()
 
 t.left(90)
 t.up()
-t.forward(26)
+t.forward(78)
 t.right(125)
 t.down()
 
 t.fillcolor("red")
 t.begin_fill()
 for i in range(2):
-  t.forward(57)
+  t.forward(171)
   t.right(110)
 t.right(15)
-t.forward(65)
+t.forward(195)
 t.end_fill()
 
 t.up()
-t.back(29)
+t.back(87)
 t.right(90)
-t.forward(7)
+t.forward(21)
 t.down()
 
 t.fillcolor("white")
+t.color("white")
 t.begin_fill()
 for i in range(5):
-  t.forward(19)
+  t.forward(57)
   t.right(144)
 t.end_fill()
+
 t.up()
-t.forward(200)
+t.forward(20)
+t.down()
+
+t.speed(3)
+t.fillcolor('white')
+t.begin_fill()
+for _ in range(5):
+  t.forward(14)
+  t.right(70)
+t.end_fill()
+
+turtle.mainloop()
